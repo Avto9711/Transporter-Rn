66388e8fec9c0a4fb8b5b276c498ea8c195c3a82
Leandro <leandrojj13@hotmail.com>
2019-03-12 13:57:01 -0400

    adding appcenter config

--- android/app/src/main/java/com/transpoterfase1/MainApplication.java
+++ android/app/src/main/java/com/transpoterfase1/MainApplication.java
@@ -3,6 +3,8 @@ package com.transpoterfase1;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.microsoft.appcenter.reactnative.analytics.AppCenterReactNativeAnalyticsPackage;
+import com.microsoft.appcenter.reactnative.appcenter.AppCenterReactNativePackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new AppCenterReactNativeAnalyticsPackage(MainApplication.this, getResources().getString(R.string.appCenterAnalytics_whenToEnableAnalytics)),
+            new AppCenterReactNativePackage(MainApplication.this),
             new RNGestureHandlerPackage()
       );
     }
