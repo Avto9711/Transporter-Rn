2caaca88ddf1f977aeb189bb433d18655b425f8c
Leandro <leandrojj13@hotmail.com>
2019-05-21 01:35:28 -0400

    profile things

--- src/pages/ProfileSetting.js
+++ src/pages/ProfileSetting.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import {StyleSheet, View, Image, ImageBackground, ScrollView} from 'react-native';
-import {Switch, Header,Left,Icon, Container, Content,Body,Title,Right, Button, Grid, Col, Text, Row, Badge , Card, CardItem, List, ListItem, Thumbnail} from 'native-base'
-import { Rating, AirbnbRating, Divider} from 'react-native-elements';
+import { StyleSheet, View, Image, ImageBackground, ScrollView } from 'react-native';
+import { Switch,Item, Input, Header, Left, Icon, Container, Content, Body, Title, Right, Button, Grid, Col, Text, Row, Badge, Card, CardItem, List, ListItem, Thumbnail, Footer } from 'native-base'
+import { Rating, AirbnbRating, Divider } from 'react-native-elements';
 import { Avatar } from 'react-native-elements';
 
 import globalColors from '../utils/colors';
 
 import { connect } from 'react-redux';
 
- class ProfileSetting extends React.Component {
+class ProfileSetting extends React.Component {
 
 
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = { _rating: 3 };
 
@@ -20,148 +20,239 @@ import { connect } from 'react-redux';
 
 
   ratingCompleted = (rating) => {
-    this.setState({_rating: rating});
+    this.setState({ _rating: rating });
   }
 
   render() {
     return (
 
-      <Container style={{flex:1}}>
-        <Header style={{backgroundColor: globalColors.baseBlue}} androidStatusBarColor={globalColors.baseBlue}>
-                <Left>
-                    <Button 
-                    transparent
-                    onPress={() => this.props.navigation.openDrawer()}
-                    >
-                    <Icon name="ios-menu" />
-                    </Button>
-                </Left>
-                <Body>
-                    <Title>Profile Settings</Title>
-                </Body>
-            <Right />
+      <Container style={{ flex: 1 }}>
+        <Header style={{ backgroundColor: globalColors.baseBlue }} androidStatusBarColor={globalColors.baseBlue}>
+          <Left>
+            <Button
+              transparent
+              onPress={() => this.props.navigation.openDrawer()}
+            >
+              <Icon name="ios-menu" />
+            </Button>
+          </Left>
+          <Body>
+            <Title>Profile Settings</Title>
+          </Body>
+          <Right />
         </Header>
-        <Content contentContainerStyle={{flex:1}}>
+        <Content contentContainerStyle={{ flex: 1 }}>
           <ScrollView>
-              <Grid>
-                  <Row style={{ backgroundColor: '#635DB7', height: 150 }}>
-                    <ImageBackground
-                          resizeMode={'stretch'} // or cover
-                          style={{flex: 1}} // must be passed from the parent, the number may vary depending upon your screen size
-                          source={{uri: 'https://scontent-mia3-2.xx.fbcdn.net/v/t1.0-9/45238751_10218432926922321_2853116813040943104_n.jpg?_nc_cat=100&_nc_ht=scontent-mia3-2.xx&oh=3aa001f707e440c918fe8871d09473ad&oe=5D19FB10'}}
-                        >
-                      <Avatar
-                      rounded
-                      style={{ position:'absolute', bottom: -37, width: 100,height: 100, left: "38%"}} 
-                            source={{
-                              uri:
-                              'https://scontent-mia3-2.xx.fbcdn.net/v/t1.0-1/p240x240/45252662_10218432882521211_5551562546558795776_n.jpg?_nc_cat=100&_nc_ht=scontent-mia3-2.xx&oh=f61915680a20a6a3e76bba24fd9866f8&oe=5D1A504D'                          }}
-                            showEditButton
-                          />
-                      </ImageBackground>
-                  </Row>
-                  <Row  style={{ marginTop: 45,  height: 130}}> 
+            <Grid>
+              <Row style={{ backgroundColor: '#635DB7', height: 150 }}>
+                <ImageBackground
+                  resizeMode={'stretch'} // or cover
+                  style={{ flex: 1 }} // must be passed from the parent, the number may vary depending upon your screen size
+                  source={{ uri: 'https://scontent-mia3-2.xx.fbcdn.net/v/t1.0-9/45238751_10218432926922321_2853116813040943104_n.jpg?_nc_cat=100&_nc_ht=scontent-mia3-2.xx&oh=3aa001f707e440c918fe8871d09473ad&oe=5D19FB10' }}
+                >
+                  <Avatar
+                    rounded
+                    style={{ position: 'absolute', bottom: -37, width: 100, height: 100, left: "38%" }}
+                    source={{
+                      uri:
+                        'https://scontent-mia3-2.xx.fbcdn.net/v/t1.0-1/p240x240/45252662_10218432882521211_5551562546558795776_n.jpg?_nc_cat=100&_nc_ht=scontent-mia3-2.xx&oh=f61915680a20a6a3e76bba24fd9866f8&oe=5D1A504D'
+                    }}
+                    showEditButton
+                  />
+                </ImageBackground>
+              </Row>
+              <Row style={{ marginTop: 45, height: 130 }}>
 
-                    <View style={{flex:1,  textAlign: 'center',  alignItems: 'center' }}>
-                        <Text style={{fontWeight: 'bold', fontSize: 25}}>Israel Rivera</Text>
-                        <Text style={{fontSize: 18}}>Joined XX on XXX - Kissimme Florida</Text>
-                        <View style={{ flex: 1, flexDirection: 'row', alignItems:'center'}}>
-                          <AirbnbRating
-                            onFinishRating={this.ratingCompleted}
-                            count={5}
-                            defaultRating={3}
-                            size={25}
-                            showRating={false}
-                          /> 
-                          <Text>({this.state._rating})</Text>
+                <View style={{ flex: 1, textAlign: 'center', alignItems: 'center' }}>
+                  <Text style={{ fontWeight: 'bold', fontSize: 25 }}>Israel Rivera</Text>
+                  <Text style={{ fontSize: 18 }}>Joined XX on XXX - Kissimme Florida</Text>
+                  <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center' }}>
+                    <AirbnbRating
+                      onFinishRating={this.ratingCompleted}
+                      count={5}
+                      defaultRating={3}
+                      size={25}
+                      showRating={false}
+                    />
+                    <Text>({this.state._rating})</Text>
+                  </View>
+                </View>
+              </Row>
+
+              <Row style={{ marginTop: 10 }}>
+                <View style={{ marginVertical: 10, flex: 1, marginHorizontal: 10 }}>
+
+                  <List style={{ marginHorizontal: 10 }}>
+                    <ListItem avatar style={{marginTop:5}} >
+                      <Left>
+                        <Icon
+                          active
+                          type={"AntDesign"}
+                          name={"user"}
+                          style={{ color: globalColors.baseBlue, fontSize: 26, width: 30 }}
+                        />
+                      </Left>
+                      <Body>
+                        <Text>Complete Name - Israel Rivera</Text>
+                      </Body>
+                      <Right>
+                        <Icon
+                          active
+                          type={"AntDesign"}
+                          name={"edit"}
+                          style={{ color: globalColors.baseBlue, fontSize: 26, width: 30 }}
+                        />
+                      </Right>
+                    </ListItem>
+                    <ListItem avatar style={{marginTop:5}} >
+                      <Left>
+                        <Icon
+                          active
+                          type={"Entypo"}
+                          name={"email"}
+                          style={{ color: globalColors.baseBlue, fontSize: 26, width: 30 }}
+                        />
+                      </Left>
+                      <Body>
+                        <Text>Email - ir@gmail.com</Text>
+                      </Body>
+                      <Right>
+                        <Icon
+                          active
+                          type={"AntDesign"}
+                          name={"edit"}
+                          style={{ color: globalColors.baseBlue, fontSize: 26, width: 30 }}
+                        />
+                      </Right>
+                    </ListItem>
+                    <ListItem avatar style={{marginTop:5}} >
+                      <Left>
+                        <Icon
+                          active
+                          type={"Entypo"}
+                          name={"address"}
+                          style={{ color: globalColors.baseBlue, fontSize: 26, width: 30 }}
+                        />
+                      </Left>
+                      <Body>
+                        <Text>Address - Kissimme, Florida </Text>
+                      </Body>
+                      <Right>
+                        <Icon
+                          active
+                          type={"AntDesign"}
+                          name={"edit"}
+                          style={{ color: globalColors.baseBlue, fontSize: 26, width: 30 }}
+                        />
+                      </Right>
+
+
+                    </ListItem>
+                    <ListItem avatar style={{marginTop:5}} >
+                      <Left>
+                        <Icon
+                          active
+                          type={"Entypo"}
+                          name={"phone"}
+                          style={{ color: globalColors.baseBlue, fontSize: 26, width: 30 }}
+                        />
+                      </Left>
+                      <Body>
+                        <Text>Phone number - ############ </Text>
+                        <Text style={styles.smallText}>We have sent you a text message with a code, write it here to verifiy your number</Text>
+
+                        <View style={{ flexDirection: 'row', marginTop: 10 }}>
+                          <View style={{ flex: 10 }}>
+                            <Item regular>
+                              <Input />
+                            </Item>
+                          </View>
+                       
+                          <View style={{ paddingLeft: 15,flex: 5 }}>
+                              <Button style={{backgroundColor: globalColors.baseBlue}} ><Text> Verify </Text></Button>
+                          </View>
                         </View>
-                      </View>
-                  </Row>
 
-                  <Row  style={{ marginTop: 15,  height: 130}}> 
-                    <View style={{marginVertical: 10, flex:1, marginHorizontal: 10 }}>
+                      </Body>
+                      <Right>
+                        <Icon
+                          active
+                          type={"AntDesign"}
+                          name={"edit"}
+                          style={{ color: globalColors.baseBlue, fontSize: 26, width: 30 }}
+                        />
+                      </Right>
 
-                      <List style={{marginHorizontal: 10}}>
-                        <ListItem avatar >
-                          <Left>
-                              <Icon
-                              active
-                              type={"AntDesign"}
-                              name={"user"}
-                              style={{ color: globalColors.baseBlue, fontSize: 26, width: 30 }}
-                            />
-                          </Left>
-                          <Body>
-                            <Text>Complete Name - Israel Rivera</Text>
-                          </Body>
-                          <Right>
-                          <Icon
-                              active
-                              type={"AntDesign"}
-                              name={"edit"}
-                              style={{ color: globalColors.baseBlue, fontSize: 26, width: 30 }}
-                            />
-                          </Right>
-                        </ListItem>
-                        <ListItem avatar >
-                          <Left>
-                              <Icon
-                              active
-                              type={"Entypo"}
-                              name={"email"}
-                              style={{ color: globalColors.baseBlue, fontSize: 26, width: 30 }}
-                            />
-                          </Left>
-                          <Body>
-                            <Text>Email - ir@gmail.com</Text>
-                          </Body>
-                          <Right>
-                          <Icon
-                              active
-                              type={"AntDesign"}
-                              name={"edit"}
-                              style={{ color: globalColors.baseBlue, fontSize: 26, width: 30 }}
-                            />
-                          </Right>
-                        </ListItem>
-                      </List>
 
-                    </View>
-                  </Row>
-              </Grid>
+                    </ListItem>
+
+                    <ListItem avatar style={{marginTop:5}} >
+                      <Left>
+                        <Icon
+                          active
+                          type={"Entypo"}
+                          name={"credit-card"}
+                          style={{ color: globalColors.baseBlue, fontSize: 26, width: 30 }}
+                        />
+                      </Left>
+                      <Body>
+                        <Text>Financial information - Card Ended - 92 11</Text>
+                      </Body>
+                      <Right>
+                        <Icon
+                          active
+                          type={"AntDesign"}
+                          name={"edit"}
+                          style={{ color: globalColors.baseBlue, fontSize: 26, width: 30 }}
+                        />
+                      </Right>
+                    </ListItem>
+                  </List>
+                 
+
+                </View>
+              </Row>
+            </Grid>
           </ScrollView>
         </Content>
-    </Container>
- 
+        <Button block style={{backgroundColor: globalColors.baseBlue, margin: 10}} ><Text> HELP </Text></Button>
+
+
+      </Container >
+
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container:{
-    backgroundColor : globalColors.baseContainerBlue,
+  container: {
+    backgroundColor: globalColors.baseContainerBlue,
     flex: 1,
     alignItems: "center",
-    justifyContent:"center"
+    justifyContent: "center"
   },
-    signupTextContainer:{
-      flexGrow: 1,
-      alignItems: 'flex-end',
-      justifyContent: 'center',
-      marginVertical: 20,
-      flexDirection: 'row',
-    },
-    signupText:{
-        color: 'rgba(255,255,255,0.6)'
-    },
-    signupLinkText:{
-        color: globalColors.white,
-        fontSize: 14,
-        fontWeight: '500',
-    }
-  });
-  
-  
+  signupTextContainer: {
+    flexGrow: 1,
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+    marginVertical: 20,
+    flexDirection: 'row',
+  },
+  signupText: {
+    color: 'rgba(255,255,255,0.6)'
+  },
+  signupLinkText: {
+    color: globalColors.white,
+    fontSize: 14,
+    fontWeight: '500',
+  },
+  smallText: {
+    fontSize: 12,
+    paddingTop: 3,
+  }
+});
+
+
 const mapStateToProps = state => ({
   token: state.token,
 });
--- src/pages/RecipientsBoard.js
+++ src/pages/RecipientsBoard.js
@@ -1,19 +1,150 @@
-import React, {Component} from 'react';
-import {View, Text} from 'react-native';
-import {Icon} from 'native-base'
+import React, { Component } from 'react';
+import { View, StyleSheet, TextInput, ScrollView, FlatList, Image, Alert, TouchableHighlight } from 'react-native';
+import globalColors from '../utils/colors'
+import ImagePicker from 'react-native-image-crop-picker';
+import {
+    Header, Left, Icon, Container, Content, Body, Title,
+    Right, Button, Text, Form,
+    Item,
+    Input,
+    Label,
+    Grid,
+    Col,
+    Row, ListItem, CheckBox, Picker, Textarea, Switch, SwipeRow, List
+} from 'native-base'
 
-class RecipientsBoard extends Component{
+function renderItemReferralLink(item, deleteReferralinkItem) {
+    return <SwipeRow
+        rightOpenValue={-75}
+        right={
+            <Button danger onPress={() => { deleteReferralinkItem(item.itemId); }}>
+                <Icon active name="trash" />
+            </Button>
+        }
+        body={
+            <View style={{ flex: 1 }}>
+                <Item regular style={{ marginBottom: 2, marginTop: 2 }}>
+                    <Input value={item.text} />
+                </Item>
+            </View>
+        }
+    />
+}
+
+class RecipientsBoard extends Component {
     static navigationOptions = {
-        title: 'Recipients Board',
-        drawerIcon: ()=>    <Icon type="FontAwesome" name="th"/>
+
     };
 
-    render(){   
-        return (
-            <View><Text>RecipientsBoard Page</Text></View>
-        )
+    constructor(props) {
+        super(props)
+        this.state = {
+            referralLinks: [{ itemId: 1, text: 'https://example.com' }, { itemId: 2, text: 'https://www.ebay.com/' }],
+            Image1: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAASwAAAEsBAMAAACLU5NGAAAAG1BMVEUAAAD///9/f3+/v7/f399fX18fHx8/Pz+fn59jyQpqAAAACXBIWXMAAA7EAAAOxAGVKw4bAAACHElEQVR4nO3VTWsaURSA4ZP5uOMy13v8WI40tF06bTBbp1riMtL8gClNocuE0q4VLP3bvXfGQGIlYMGxi/eBeIZ4Ql7GmVEEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIB/ExWzm+ao3HnnbPrCX50fq2er0NI1R0+ykvBfX8pKjpyVusqUzek6IOtrceSsuC/ysKoPD8ha2xayOrnZLEJWPdLio7G2CllJ/3L1Xf25vNBcTDHRZkPkx+2RszrD8HrnirHPqsdoVt6sw299lr51w9ueRLrp+jdn2mzI8a+tzLo3IpsqHfisMIxWyXj7ISZdsXnmJOkbJ6M81maxhSz5Vtpr6Yp88FlhRP7kTB+z+lJWohLlRqWoMm0W28gS88n+1PncSWnCCBfb4yXvh78P1N95r1TmYrTeaCdLZPnbWqtSZmHE539nfbELDXVab7SRlUz9z1UvHJZZGPvOVvHav/gbVeuNNrJCxdnEXzK//IcYRmcgnelOln8wNNdWvdFGVuSf8uu8qNKeLwjD33ifVztZZRWpLFex1httZIXvRJWH5rlVj/XM3sfuWdZo8b7MYzffLraRFRXOP8EvF1W4eMKIiivJ5s+y0nKyHMvmWpuNNrIOYNypC/a4uE+Hp27YY7m6G5y6YY/E2vGpG/Ywm3enTgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA/Jf+ABh/R+zTl92rAAAAAElFTkSuQmCC",
+            Image2: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAASwAAAEsBAMAAACLU5NGAAAAG1BMVEUAAAD///9/f3+/v7/f399fX18fHx8/Pz+fn59jyQpqAAAACXBIWXMAAA7EAAAOxAGVKw4bAAACPUlEQVR4nO3VwW8SQRTH8ddlWTh2mCn0uMRGPbLS0CsjmHKEqPc11sQjjYlnSNb4b/tmlyaWogmNEzx8Pwm8Dfs2/Nh5O4gAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAPA8STFbNkd+78zZ9I8XZdWsjJRnp3DeNke/xUrP5a+xUlv0o6Zq2zLzy2NjDfPkMmqslv7q+82xsYpSXPRY3TyrFiFWXdrFu8yYMsRK+9ebb07v5ZXLJSsmrukQmT+dxH+rWy/GnS1G+kV1Gc78chs+1Vjutb38cCGJq3p6cuaaDpGPEvludYx9JVKV7YHGCiVzZTraLWLaE5N3rKT9zOo8tVzTGESeLfnqza30RN5qrFASvTnTh1h98WGKkjxzOk8d1zQGw7hPou5B7813N59b8VkoYdgeRl6Lr5frywun85S5uiNc1HXryLFEVj+NMU58J5TW+dNYn83ChXSu7gg/xU/iRkqn+rq5CIe+E8qhu1W81Dd9UF3dEZoiT1ad4myiI/NDFzGU7kC6071YujE0s1V36KnVJnKsRHf5bV6U7QtNEIo+eJ82e7F8mTiN0nJ1h9TbaWT6n+jkvtm36rKdmXXLPoo1XIx93rLzXaOu93g8jhsrKazu4NeLMgxPKElxI535o1htP1mNpLp1TYeITr6JG+sImT11ggOu1u3Yz99zrDZ3g1NnOCA1ZnTqDAdk1ZtTRwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA/Jd+AfFXTEtoBIyvAAAAAElFTkSuQmCC",
+            Image3: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAASwAAAEsBAMAAACLU5NGAAAAG1BMVEUAAAD///9/f3+/v7/f399fX18fHx8/Pz+fn59jyQpqAAAACXBIWXMAAA7EAAAOxAGVKw4bAAACQ0lEQVR4nO3WwYvaQBTH8bdJTDzumFH3GOnS9mjaxb06Na0elfYPyLJb6HGXQs8Klv7bfZPswq61BYXBHr4f0BfMC/wyzkwiAgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADAcaJytmyP3M6Zs+lfL8rKRR0qUKu0Lm+PnsVKzuWfsR5yNwiaqpPXqVseGmtTRxdBY8V61w/rQ2MtJM2Dx+oW6XbhYzWlU35Mjal9rGRwtf5udSwvbSFpObFth8jP0LG6zZ9xl5djjdWU0cwtN/5XjWXf5hdf+hLZbU9Pzmzb0VzXDxorM/kbkW3dGWosX1JbJ+PHPzHpiSmyXJKBDs6oiG3b6O/DFEFjyTdn5tIT+aCxfIl0cKZPsQbiarESFamVss5s26huzKewsST9bH7YqsrFpb74yfY05bXoOrAiX19ZqSS1TUdz1W0vcCyR1S9jjBWX+RKf/xnr1iysT2ebjvZmwk75ZKqf62b+usyXfaNVvtYvXag2ayd6NhcdvJB8irNJz695l/rSHUp3uhNLN4Z2bjUdOlSaLuxoRbrLb4qy7vQ1gS+68G7WO7FcHVlZrWPbdOipqo4Czy19Jlp9xjX7VlM2M3Mf5y9ijRbvXRHn1WOjzsaZm4aNFZW57uBX+kbg2hKV15JVL2J13GQ1lu3cth36JHCh3yAOEHj1HefyvhP2reE4q/Xd8NQZ9kiMGZ86wx7p9t2pIwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA/ku/AfmgTgUbnWwzAAAAAElFTkSuQmCC",
+        }
+        this.deleteReferralinkItem = this.deleteReferralinkItem.bind(this);
+    }
+
+    insertImage = (imageNumber) => {
+        ImagePicker.openCamera({
+            width: 300,
+            height: 400,
+            cropping: true,
+        }).then(image => {
+            console.log(image);
+            this.setState(state => ({
+                ['Image' + imageNumber]: image.path
+            }));
+        });
+    }
+    deleteReferralinkItem(itemId) {
+        //When using React, you should never mutate the state directly. 
+        //If an object (or Array, which is an object too) is changed, you should create a new copy.
+        const { referralLinks } = this.state;
+        this.setState(state => ({
+            referralLinks: referralLinks.filter(x => x.itemId != itemId)
+        }));
 
     }
+
+    componentWillUpdate(nextProps, nextState) {
+    }
+
+    showAlert = () => {
+        Alert.alert(
+            'Alert Title',
+            'My Alert Msg',
+            [
+                { text: 'Ask me later', onPress: () => console.log('Ask me later pressed') },
+                {
+                    text: 'Cancel',
+                    onPress: () => console.log('Cancel Pressed'),
+                    style: 'cancel',
+                },
+                { text: 'OK', onPress: () => console.log('OK Pressed') },
+            ],
+            { cancelable: false },
+        );
+    }
+
+
+    render() {
+        return (
+            <Container style={{ flex: 1 }}>
+                <Header style={{ backgroundColor: globalColors.baseBlue }} androidStatusBarColor={globalColors.baseBlue}>
+                    <Left>
+                        <Button
+                            transparent
+                            onPress={() => this.props.navigation.openDrawer()}
+                        >
+                            <Icon name="ios-menu" />
+                        </Button>
+                    </Left>
+                    <Body>
+                        <Title>Recipients</Title>
+                    </Body>
+                    <Right>
+                        {/* <Button transparent>
+                <Icon name="search" />
+                </Button> */}
+                    </Right>
+                </Header>
+                <Content contentContainerStyle={{ flex: 1, padding: 10 }} >
+                    <ScrollView>
+
+
+                    </ScrollView>
+
+
+                </Content>
+            </Container>)
+    }
+
+
 }
+
+const styles = StyleSheet.create({
+    searchSection: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+    },
+    searchIcon: {
+        padding: 10,
+    },
+    input: {
+        flex: 1,
+        paddingTop: 10,
+        paddingRight: 10,
+        paddingBottom: 10,
+        paddingLeft: 0,
+        backgroundColor: '#fff',
+        color: '#424242',
+    },
+})
 
 export default RecipientsBoard
