e5bd5638209390dcfa424cb0033e8a9c705e78ac
Leandro <leandrojj13@hotmail.com>
2019-08-27 20:42:21 -0400

    messages

---
+++ src/pages/MessageDetails.js
@@ -0,0 +1,111 @@
+import React from "react";
+import { AsyncStorage, StatusBar, StyleSheet, View } from "react-native";
+
+import { connect } from "react-redux";
+
+import {
+  Header,
+  Left,
+  Icon,
+  Container,
+  Content,
+  Body,
+  Title,
+  Right,
+  Button,
+  Text
+} from "native-base";
+
+import globalColors from "../utils/colors";
+import { GiftedChat } from "react-native-gifted-chat";
+import { AppStack } from "../utils/screens";
+
+class MessageDetails extends React.Component {
+  static navigationOptions = {};
+  componentWillMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: "Hello developer",
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: "React Native",
+            avatar: "https://placeimg.com/140/140/any"
+          },
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/a/af/Tour_eiffel_at_sunrise_from_the_trocadero.jpg"
+        }
+      ]
+    });
+  }
+
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages)
+    }));
+  }
+
+  render() {
+    return (
+      <Container style={{ flex: 1 }}>
+        <Header
+          style={{ backgroundColor: globalColors.baseBlue }}
+          androidStatusBarColor={globalColors.baseBlue}
+        >
+          <Left>
+            <Button
+              transparent
+              onPress={() => this.props.navigation.navigate(AppStack.Message)}
+            >
+              <Icon name="arrow-back" />
+            </Button>
+          </Left>
+          <Body>
+            <Title>Amy Farha</Title>
+          </Body>
+          <Right>
+            <Button
+              transparent
+            >
+              <Icon type="FontAwesome" name="ellipsis-v" />
+            </Button>
+          </Right>
+        </Header>
+        <Content
+          contentContainerStyle={{
+            flex: 1,
+          }}
+        >
+          <GiftedChat
+            messages={this.state.messages}
+            onSend={messages => this.onSend(messages)}
+            user={{
+              _id: 1
+            }}
+          />
+        </Content>
+      </Container>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center"
+  }
+});
+
+const mapStateToProps = state => ({
+  token: state.token
+});
+
+const mapDispatchToProps = dispatch => ({});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(MessageDetails);
--- src/pages/Messages.js
+++ src/pages/Messages.js
@@ -1,19 +1,152 @@
-import React, {Component} from 'react';
-import {View, Text} from 'react-native';
-import {Icon} from 'native-base'
+import React, { Component } from "react";
+import { View } from "react-native";
+import globalColors from "../utils/colors";
+import {
+  Header,
+  Left,
+  Icon,
+  Container,
+  Content,
+  Body,
+  Title,
+  Right,
+  Button,
+  Text
+} from "native-base";
+import { ListItem } from "react-native-elements";
+import { AppStack } from "../utils/screens";
 
-class Messages extends Component{
-    static navigationOptions = {
-        title: 'Messages',
-        drawerIcon: ()=>    <Icon type="FontAwesome" name="comments-o"/>
-    };
+import { connect } from "react-redux";
 
-    render(){   
-        return (
-            <View><Text>Messages Page</Text></View>
-        )
+class Messages extends Component {
+  static navigationOptions = {};
+  constructor(props) {
+    super(props);
+  }
+  state = {
+    list: [
+      {
+        name: "Amy Farha",
+        avatar_url:
+          "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg",
+        subtitle: "Vice President"
+      },
+      {
+        name: "Chris Jackson",
+        avatar_url:
+          "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg",
+        subtitle: "Vice Chairman"
+      },
+      {
+        name: "Amy Farha",
+        avatar_url:
+          "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg",
+        subtitle: "Vice President"
+      },
+      {
+        name: "Chris Jackson",
+        avatar_url:
+          "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg",
+        subtitle: "Vice Chairman"
+      },
+      {
+        name: "Amy Farha",
+        avatar_url:
+          "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg",
+        subtitle: "Vice President"
+      },
+      {
+        name: "Amy Farha",
+        avatar_url:
+          "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg",
+        subtitle: "Vice President"
+      },
+      {
+        name: "Chris Jackson",
+        avatar_url:
+          "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg",
+        subtitle: "Vice Chairman"
+      },
+      {
+        name: "Amy Farha",
+        avatar_url:
+          "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg",
+        subtitle: "Vice President"
+      },
+      {
+        name: "Chris Jackson",
+        avatar_url:
+          "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg",
+        subtitle: "Vice Chairman"
+      },
+      {
+        name: "Amy Farha",
+        avatar_url:
+          "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg",
+        subtitle: "Vice President"
+      }
+    ]
+  };
 
-    }
+  redirectToDetail = () => {
+    this.props.navigation.navigate(AppStack.MessageDetails);
+  };
+
+  render() {
+    return (
+      <Container style={{ flex: 1 }}>
+        <Header
+          style={{ backgroundColor: globalColors.baseBlue }}
+          androidStatusBarColor={globalColors.baseBlue}
+        >
+          <Left>
+            <Button
+              transparent
+              onPress={() => this.props.navigation.openDrawer()}
+            >
+              <Icon name="ios-menu" />
+            </Button>
+          </Left>
+          <Body>
+            <Title>Messages </Title>
+          </Body>
+          <Right>
+            <Button
+              transparent
+            >
+              <Icon type="FontAwesome" name="edit" />
+            </Button>
+          </Right>
+        </Header>
+        <Content>
+          <View>
+            {this.state.list.map((l, i) => (
+              <ListItem
+                onPress={() => {
+                  this.redirectToDetail();
+                }}
+                key={i}
+                leftAvatar={{ source: { uri: l.avatar_url } }}
+                title={l.name}
+                subtitle={l.subtitle}
+              />
+            ))}
+          </View>
+        </Content>
+      </Container>
+    );
+  }
 }
 
-export default Messages
+const mapStateToProps = state => ({
+  token: state.token
+});
+
+const mapDispatchToProps = dispatch => ({
+  //removeUserToken: () => dispatch(removeUserToken()),
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Messages);
